2a167768dd638250dca4851915ef23ed626586eb
unknown <shafiulalam50.sa@gmail.com>
2024-01-19 23:10:31 +0600

    13

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const dbConnection = ()=> {
-    mongoose.connect(process.env.MONGO_URI.toString(), {
+    mongoose.connect(process.env.MONGO_URI, {
         dbName: "RESTAURANT"
     }).then(()=> {
         console.log("Connected to database successfully")
