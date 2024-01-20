1c398152788a65cf534a1b76b0e2f47f34d25a8a
unknown <shafiulalam50.sa@gmail.com>
2024-01-20 11:53:59 +0600

    again

--- frontend/components/Reservation.jsx
+++ frontend/components/Reservation.jsx
@@ -17,7 +17,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://mern-stack-restaurant-app.onrender.com/api/v1/reservation/send",
+        "http://localhost:4000/api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
